8e6bdf8dbc49da310d881fe694532c03564dc83b
zafran-ullah <zafranullah7@gmail.com>
2025-08-12 00:41:50 +0500

    VM SSH Issue

--- infra/terraform/modules/vm/main.tf
+++ infra/terraform/modules/vm/main.tf
@@ -46,6 +46,18 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_public_ip" "main" {
